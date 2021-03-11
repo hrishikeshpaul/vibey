0ffb56640ae48185f6f6ca70a4c8450d3e37dee3
Hrishikesh Paul <hrpaul@iu.edu>
2021-03-11 15:47:47 -0500

    chore: added bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,20 +5,7 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className="btn btn-primary text-white">Hello</button>
     </div>
   );
 }
