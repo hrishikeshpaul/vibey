5bb939a88bd20e55a9e68f88c9917aa93a7e59c6
s-munro <68410363+s-munro@users.noreply.github.com>
2021-03-11 10:49:09 -0500

    chore: logout destroys session, test-session page added

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,17 +1,22 @@
-
 import { generateRandomString, scopes, STATE_KEY } from "../static/const";
 import { initSpotifyApi } from "../common/spotify";
-const User = require("../db/mongo/models/user")
-const app = require('express')();
+const User = require("../db/mongo/models/user");
+const app = require("express")();
 const spotifyApi = new initSpotifyApi();
 
 /**
- * Get authorization code from Spotify by 
+ * Get authorization code from Spotify by
  * using the Client ID and Secret
- * 
+ *
  * Redirects to call back which then gets the access token and refresh token
  */
-app.get('/login', (_, res) => {
+app.get("/login", (req, res) => {
+  if (req.session === null) {
+    console.log("no session");
+  } else {
+    console.log("session active");
+    console.log("session: ", req.session);
+  }
   const state = generateRandomString(16);
   res.cookie(STATE_KEY, state);
   const authorizeURL = spotifyApi.createAuthorizeURL(scopes, state);
@@ -21,14 +26,14 @@ app.get('/login', (_, res) => {
 /**
  * Requests Spotify for access token and refresh token
  * Sets the session
- * 
+ *
  */
-app.get('/callback', async (req, res) => {
+app.get("/callback", async (req, res) => {
   const { code, state } = req.query;
   const storedState = req.cookies ? req.cookies[STATE_KEY] : null;
 
   if (state === null || state !== storedState) {
-    res.redirect('http://localhost:5555/error?msg=state_mismatch');
+    res.redirect("http://localhost:5555/error?msg=state_mismatch");
   } else {
     try {
       const data = await spotifyApi.authorizationCodeGrant(code);
@@ -44,17 +49,31 @@ app.get('/callback', async (req, res) => {
         email: user.body.email,
         href: user.body.href,
         uri: user.body.uri,
-        image: user.body.images.length > 0 ? user.body.images[0].url : null
-      }
+        image: user.body.images.length > 0 ? user.body.images[0].url : null,
+      };
+      // Seems like this following code should only run if user doesn't already exist
       const savedUser = await new User(userObj).save();
-      res.send(savedUser)
-    } catch(err) {
-        res.send(err)
+      res.send(savedUser);
+    } catch (err) {
+      res.send(err);
     }
   }
 });
 
+/**
+ * Destroys the current session (redirects, so a new session is created
+ *    but missing access token and refresh token)
+ * TODO: reset spotifyApi state
+ * Redirects to home page, where a new session is created (new session lacks tokens)
+ *
+ */
+app.get("/logout", async (req, res) => {
+  req.session.destroy(function () {
+    res.redirect("http://localhost:5555/");
+  });
+});
+
 export default {
   app,
-  spotifyApi
+  spotifyApi,
 };
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,36 +1,63 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const spotifyApi = require('./auth').default.spotifyApi;
+const spotifyApi = require("./auth").default.spotifyApi;
 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  console.log("req session: ", req.session);
+  res.render("index", { title: "Express" });
 });
 
-router.get('/me', async (req, res, next) => {
+router.get("/me", async (req, res, next) => {
   spotifyApi.setAccessToken(req.session.access_token);
   spotifyApi.setRefreshToken(req.session.refresh_token);
-  spotifyApi.getMe()
+  spotifyApi
+    .getMe()
     .then(({ body }) => {
-      res.send(body)
+      res.send(body);
     })
     .catch((error) => {
-      console.log(error)
-      res.send(error)
-    })
-}); 
+      console.log(error);
+      res.send(error);
+    });
+});
 
-router.get('/getPlaylist', (req, res, next) => {
-  spotifyApi.getUserPlaylists('nutkesh')
-  .then(function(data) {
-    res.send(data)
-  },function(err) {
-    res.status(err.body.error.status).send(err)
+router.get("/getPlaylist", (req, res, next) => {
+  spotifyApi.getUserPlaylists("nutkesh").then(
+    function (data) {
+      res.send(data);
+    },
+    function (err) {
+      res.status(err.body.error.status).send(err);
+    }
+  );
+});
+
+const User = require("../db/mongo/models/user");
+router.get("/hi", (req, res) => {
+  User.find({}, (err, users) => {
+    res.send(users);
   });
-})
+});
 
-router.get('/error', (req, res) => {
-  res.send(req.query)
-})
+router.get("/test-session", async (req, res) => {
+  if (!req.session.access_token) {
+    if (!req.session.user) {
+      res.send("doesnt contain token or user");
+    } else {
+      res.send("session doesn't contain access token but has user");
+    }
+  } else {
+    if (!req.session.user) {
+      res.send("contains token but not user");
+    } else {
+      res.send("contains token AND user");
+    }
+  }
+});
+
+router.get("/error", (req, res) => {
+  res.send(req.query);
+});
 
 module.exports = router;
