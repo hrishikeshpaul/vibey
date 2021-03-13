6b6751c7481c1ae6a85797fcc2d0862b0b5f254c
s-munro <68410363+s-munro@users.noreply.github.com>
2021-03-13 02:58:00 -0500

    ref: setting up socket in own module

--- backend/src/common/rooms.js
+++ backend/src/common/rooms.js
@@ -1,1 +1,20 @@
+// var socket_io = require("socket.io");
+// var io = socket_io();
+// var socketApi = {};
 
+// socketApi.io = io;
+
+// io.on("connection", function (socket) {
+//   console.log("connected baybeeeee!");
+// });
+
+// module.exports = socketApi;
+
+module.exports = function (io) {
+  io.on("connection", (socket) => {
+    console.log("we out here");
+    socket.on("disconnect", () => {
+      console.log("disconnected");
+    });
+  });
+};
