40403b8c4d67cf045152eb5bde7985fd7366c2ab
s-munro <68410363+s-munro@users.noreply.github.com>
2021-03-13 01:52:18 -0500

    ref: socket moved back to www

--- backend/src/common/rooms.js
+++ backend/src/common/rooms.js
@@ -1,17 +1,1 @@
-const io = require("../../bin/www");
 
-/*
- * Very far-back import with path, since bin is behind src
- * from my understanding, if we want to keep the server in www then we have
- * to pull something back from www to make the sockets work here,
- * whether it's io or server
- *
- */
-
-io.on("connection", (socket) => {
-  console.log("new connection found!!!");
-
-  socket.on("disconnect", () => {
-    console.log("the connection disconnected!!");
-  });
-});
