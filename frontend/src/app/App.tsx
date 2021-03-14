557619c3ab4bc2df788184de463e8db25aa812fe
s-munro <68410363+s-munro@users.noreply.github.com>
2021-03-14 05:54:22 -0400

    ref: removed rooms references on front-end for this branch

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -4,7 +4,6 @@ import Redirect from "app/modules/redirect/Redirect";
 
 import "./App.scss";
 import Home from "./modules/home/home";
-import Room from "./modules/room/room";
 
 const App = () => {
   return (
@@ -13,7 +12,6 @@ const App = () => {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/login" component={Redirect} />
-          <Route path="/room/:id" component={Room} />
         </Switch>
       </Router>
     </div>
