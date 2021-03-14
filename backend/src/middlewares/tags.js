e3cb752c21e7267e8ebf4ea3de77a2ab520500a8
s-munro <68410363+s-munro@users.noreply.github.com>
2021-03-14 05:44:08 -0400

    ref: refactored tags middleware error handling

--- backend/src/middlewares/tags.js
+++ backend/src/middlewares/tags.js
@@ -1,29 +1,27 @@
 /**
  * We could go really extra with these,
- * making sure chars are string not integer, english chars, etc
+ * i.e. making sure chars are string (not integer), english chars only, etc
  *
  */
 
 /**
  * If not tag name, return 400
  */
-
 export function validateTagName(req, res, next) {
   if (req.query.name) {
     return next();
   } else {
-    return res.status(400).send("Tag name required");
+    return res.status(400).json({ error: "Tag name required" });
   }
 }
 
 /**
  * Max tag length 20 characters.
  */
-
 export function validateTagLength(req, res, next) {
   if (req.query.name.length < 20) {
     return next();
   } else {
-    return res.status(400).send("Maximum length reached");
+    return res.status(400).json({ error: "Tag name exceeded maximum length" });
   }
 }
