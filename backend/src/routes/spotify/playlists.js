e4df8a5faf1ecd46cd6e1cb09484e6ccfa309351
s-munro <68410363+s-munro@users.noreply.github.com>
2021-03-14 21:20:24 -0400

    chore: playlists routes functioning

--- backend/src/routes/spotify/playlists.js
+++ backend/src/routes/spotify/playlists.js
@@ -1,5 +1,9 @@
 import { isLoggedIn } from "../../middlewares/auth";
 import { spotifyApi } from "../../common/spotify";
+import {
+  validateGetUserPlaylistsQuery,
+  validatePlaylistId,
+} from "../../middlewares/playlists";
 
 var express = require("express");
 var router = express.Router();
@@ -7,28 +11,39 @@ var router = express.Router();
 /**
  * /api/spotify/playlists/all
  * Get all playlists for current user
- * limit and offset necessary
+ * username, limit and offset necessary
+ * RELEVANT ENDPOINT: https://api.spotify.com/v1/me/playlists
+ * Not sure how we want to handle passing in username
  */
-router.get("/all", async function (req, res, next) {
-  const { limit, offset } = req.query;
-  try {
-    const result = await spotifyApi.getUserPlaylists("sjmunro-us");
-    res.status(200).json(result);
-  } catch (err) {
-    console.log("err: ", err);
-    res.status(500).json({ err: err });
+router.get(
+  "/all",
+  validateGetUserPlaylistsQuery,
+  async function (req, res, next) {
+    const { limit, offset, username } = req.query;
+    try {
+      const response = await spotifyApi.getUserPlaylists(username, {
+        limit,
+        offset,
+      });
+      res.status(200).json(response);
+    } catch (err) {
+      res.status(500).json({ err });
+    }
   }
-  // https://api.spotify.com/v1/me/playlists
-  // res.status(200).json("there");
-});
+);
 
 /**
- * /api/spotify/playlists/id
+ * /api/spotify/playlists/playlist?id={id}
  * Get a specific playlist from current user
  */
-router.get("/:id", function (req, res, next) {
-  const { id } = req.params;
-  res.status(200).json(id);
+router.get("/playlist", validatePlaylistId, async function (req, res, next) {
+  const { id } = req.query;
+  try {
+    const response = await spotifyApi.getPlaylist(id);
+    res.status(200).json(response);
+  } catch (err) {
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
 module.exports = router;
