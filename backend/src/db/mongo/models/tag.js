364f453aefc2e349a35e4e5ef3b06a165179318e
s-munro <68410363+s-munro@users.noreply.github.com>
2021-03-14 23:00:01 -0400

    fix: spacing

--- backend/src/db/mongo/models/tag.js
+++ backend/src/db/mongo/models/tag.js
@@ -18,7 +18,7 @@ const TagSchema = mongoose.Schema({
      * collation: treat different characters as the same character
      *    locale: establishes that ENGLISH characters are considered to be the same
      *    strength: 2 (1 and 2 indicate case-insensitive)
-     *     https://docs.mongodb.com/manual/core/index-case-insensitive/
+     *    https://docs.mongodb.com/manual/core/index-case-insensitive/
      */
     index: {
       unique: true,
