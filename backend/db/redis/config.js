d75c1b42a5953a16435b8ce5de3a706dda0f4907
Hrishikesh Paul <hrpaul@iu.edu>
2021-04-01 00:09:32 -0400

    chore: added workflow

--- backend/db/redis/config.js
+++
@@ -1,9 +0,0 @@
-const redis = require("redis");
-
-module.exports = function connetRedis() {
-  const client = redis.createClient();
-
-  client.on("error", function(error) {
-    console.error(error);
-  });
-}
