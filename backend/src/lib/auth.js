8918e57734439dd57df61c98c8d0b1e3183392c5
Hrishikesh Paul <hrpaul@iu.edu>
2021-04-05 23:00:29 -0400

    chore: added jwt auth for be

--- backend/src/lib/auth.js
+++ backend/src/lib/auth.js
@@ -1,11 +1,37 @@
 'use strict';
 
+const jwt = require('jsonwebtoken');
+
+const jwtSecret = process.env.JWT_SECRET;
+
 const setSession = (req, accessToken, refreshToken, user) => {
   req.session.access_token = accessToken;
   req.session.refresh_token = refreshToken;
   req.session.user = user;
 };
 
+/**
+ * Generates a JWWT token
+ * @param {object} user user object
+ */
+
+const generateToken = (user) => {
+  const { email, id } = user;
+  const payload = {
+    subject: id,
+    email: email,
+    role: 'user',
+  };
+
+  const options = {
+    issuer: 'vibey',
+    expiredIn: '1h',
+  };
+
+  return jwt.sign(payload, jwtSecret, options);
+};
+
 module.exports = {
   setSession,
+  generateToken,
 };
--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -1,8 +1,19 @@
 'use strict';
 
+const jwt = require('jsonwebtoken');
+const jwtSecret = process.env.JWT_SECRET;
+
 const isLoggedIn = (req, res, next) => {
-  if (req.session.user) {
-    return next();
+  const token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token, jwtSecret, (err, decoded) => {
+      if (err) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      } else {
+        req.decodedJwt = decoded;
+        next();
+      }
+    });
   } else {
     return res.status(401).json({ error: 'Unauthorized' });
   }
