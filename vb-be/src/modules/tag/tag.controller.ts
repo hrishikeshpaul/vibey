c7eca6eccb5f89a8df09cf42d75323ccbc1bce04
s-munro <sjm.munro@gmail.com>
2021-08-06 20:17:43 -0400

    chore: search tags endpoint and find service

--- vb-be/src/modules/tag/tag.controller.ts
+++ vb-be/src/modules/tag/tag.controller.ts
@@ -1,10 +1,9 @@
-import { Controller, Get, Response, Request, Query } from '@nestjs/common';
+import { Controller, Get, Response, Request } from '@nestjs/common';
 import { Response as Res, Request as Req } from 'express';
 
 import { HttpStatus } from 'src/util/http';
 import { ErrorHandler, ErrorText } from 'src/util/error';
 
-import { TagModel, TagType } from './tag.schema';
 import { TagService } from './tag.service';
 
 @Controller('/api/tag')
@@ -12,31 +11,20 @@ export class TagController {
   constructor(private readonly tagService: TagService) {}
 
   @Get('/search')
-  async search(
-    @Request() req: Req,
-    @Response() res: Res,
-  ): Promise<Array<TagType>> {
+  async search(@Request() req: Req, @Response() res: Res) {
     const { str } = req.query;
 
     if (typeof str !== 'string') {
-      res
+      return res
         .status(HttpStatus.Error)
         .send(new ErrorHandler(HttpStatus.Error, ErrorText.InvalidDataSet));
-      return;
     }
 
-    TagModel.find(
-      { label: { $regex: str, $options: 'i' } },
-      { label: 1, score: 1, value: 1, _id: 0 },
-      function (err, docs) {
-        if (err) {
-          res.status(500).json({ error: 'Error finding results' });
-          return;
-        } else {
-          res.status(200).json(docs);
-          return;
-        }
-      },
-    );
+    try {
+      const queryResults = await this.tagService.find(str);
+      return res.status(200).json(queryResults);
+    } catch (err) {
+      throw new ErrorHandler(HttpStatus.InternalError, ErrorText.Generic);
+    }
   }
 }
