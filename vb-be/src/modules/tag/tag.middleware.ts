2b330a0d7f851b01380441b0a79b6592174fa569
Hrishikesh Paul <hrpaul@iu.edu>
2021-08-07 12:42:07 -0500

    chore: small fixes

--- vb-be/src/modules/tag/tag.middleware.ts
+++ vb-be/src/modules/tag/tag.middleware.ts
@@ -7,11 +7,7 @@ import { ErrorText } from 'src/util/error';
 @Injectable()
 export class SearchTagMiddleware implements NestMiddleware {
   async use(req: Request, res: Response, next: NextFunction) {
-    if (
-      req.query.label &&
-      typeof req.query.label === 'string' &&
-      req.query.label.length < 13
-    ) {
+    if (typeof req.query.label === 'string' && req.query.label.length < 13) {
       return next();
     }
     return res
