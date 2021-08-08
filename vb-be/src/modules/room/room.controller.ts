ff1460e32f6ad36ba5d8555b6978474732896e9d
s-munro <sjm.munro@gmail.com>
2021-08-08 02:41:07 -0400

    ref: try catch block for post room

--- vb-be/src/modules/room/room.controller.ts
+++ vb-be/src/modules/room/room.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Post, Response, Body } from '@nestjs/common';
 import { Response as Res } from 'express';
 
 import { HttpStatus } from 'src/util/http';
-import { ErrorText } from 'src/util/error';
+import { ErrorHandler, ErrorText } from 'src/util/error';
 
 import { RoomService } from '@modules/room/room.service';
 import { ICreateRoom } from '@modules/room/room.constants';
@@ -18,37 +18,42 @@ export class RoomController {
   @Post('/')
   async create(@Body() body: { roomObj: ICreateRoom }, @Response() res: Res) {
     const { roomObj } = body;
-    if (!roomObj) {
-      return res
-        .status(HttpStatus.Error)
-        .json({ error: ErrorText.InvalidDataSet });
-    }
-
-    const tagsArr = [];
-    for (const tag of roomObj.tags) {
-      if (tag.__isNew__) {
-        const newTag = await this.tagService.create(tag.label);
-        tagsArr.push(newTag._id);
-      } else {
-        const updatedTag = await this.tagService.updateScore(tag._id);
-        tagsArr.push(updatedTag._id);
-      }
-    }
-
-    const roomData = {
-      name: roomObj.name,
-      description: roomObj.description,
-      tags: tagsArr,
-      host: roomObj.host,
-    };
 
     try {
-      const room = await this.roomService.create(roomData);
-      const populatedRoom = await this.roomService.getOneRoom(room._id);
+      if (!roomObj) {
+        throw new ErrorHandler(HttpStatus.Error, ErrorText.InvalidDataSet);
+      }
 
-      res.status(HttpStatus.NewResource).json(populatedRoom);
+      const tagsArr = [];
+      for (const tag of roomObj.tags) {
+        if (tag.__isNew__) {
+          const newTag = await this.tagService.create(tag.label);
+          tagsArr.push(newTag._id);
+        } else if (tag._id) {
+          const updatedTag = await this.tagService.updateScore(tag._id);
+          tagsArr.push(updatedTag._id);
+        } else {
+          throw new ErrorHandler(HttpStatus.Error, ErrorText.InvalidDataSet);
+        }
+      }
+
+      const roomData = {
+        name: roomObj.name,
+        description: roomObj.description,
+        tags: tagsArr,
+        host: roomObj.host,
+      };
+
+      try {
+        const room = await this.roomService.create(roomData);
+        const populatedRoom = await this.roomService.getOneRoom(room._id);
+
+        return res.status(HttpStatus.NewResource).json(populatedRoom);
+      } catch (err) {
+        throw new ErrorHandler(HttpStatus.InternalError, ErrorText.Generic);
+      }
     } catch (err) {
-      res.status(HttpStatus.InternalError).json({ error: ErrorText.Generic });
+      return res.status(err.statusCode).send(err.message);
     }
   }
 }
