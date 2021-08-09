3769fed047ed9caf4d26fb543c50b592b5978e57
s-munro <sjm.munro@gmail.com>
2021-08-08 22:48:42 -0400

    chore: app.tsx refactored for pushing history

--- vb-fe/src/App.tsx
+++ vb-fe/src/App.tsx
@@ -1,26 +1,28 @@
 import React, { useEffect, useMemo } from "react";
 
 import { useDispatch, useSelector } from "react-redux";
-import { Switch, Route, useHistory } from "react-router-dom";
+import { Switch, Route, useHistory, useLocation } from "react-router-dom";
 
 import { Home } from "core/home/Home";
 import { Landing } from "core/landing/Landing";
 import { Redirect } from "core/redirect/Redirect";
+import { useSocket } from "core/socket/useSocket";
 import { Loading, CreateRoom } from "components/index";
 import { State } from "_store/rootReducer";
 import { SystemConstants } from "_store/system/SystemTypes";
+import { createRoomAction } from "_store/room/RoomActions";
+import { RoomForm } from "util/Room";
 
 import "App.scss";
-import { useSocket } from "core/socket/useSocket";
-import { RoomForm } from "util/Room";
-import { createRoomAction } from "_store/room/RoomActions";
 
 export const App = (): JSX.Element => {
   const isLoading = useSelector((state: State) => state.system.isLoading);
   const isCreateRoomModalOpen = useSelector((state: State) => state.system.createRoomOpen);
   const isAuthenticated = useSelector((state: State) => state.system.isAuthenticated);
+  const currentRoom = useSelector((state: State) => state.room.currentRoom);
   const dispatch = useDispatch();
   const history = useHistory();
+  const location = useLocation();
 
   /**
    * If at and rt are present then get profile
@@ -64,7 +66,7 @@ export const App = (): JSX.Element => {
 
     return (
       <Switch>
-        <Route path="/room">
+        <Route path="/room/:id">
           <div>This is a room!</div>
         </Route>
         <Route path="/">
@@ -78,18 +80,11 @@ export const App = (): JSX.Element => {
     return isAuthenticated ? <AuthenticatedApp /> : <UnauthenticatedApp />;
   }, [isAuthenticated]);
 
-  const handleCloseModal = () => {
-    dispatch({
-      type: SystemConstants.CREATE_ROOM_MODAL,
-      payload: false,
-    });
-  };
-
-  const handleSubmitRoom = async (room: RoomForm) => {
-    dispatch(createRoomAction(room));
-    handleCloseModal();
-    history.push("/room");
-  };
+  useEffect(() => {
+    if (currentRoom && location.pathname !== `/room/${currentRoom._id}`) {
+      history.push(`/room/${currentRoom._id}`);
+    }
+  }, [currentRoom, history, location.pathname]);
 
   return (
     <div className="h-100 w-100 px-3">
@@ -97,9 +92,9 @@ export const App = (): JSX.Element => {
       {isCreateRoomModalOpen && (
         <CreateRoom
           open={isCreateRoomModalOpen}
-          close={handleCloseModal}
+          close={() => dispatch({ type: SystemConstants.CREATE_ROOM_MODAL, payload: false })}
           submit={(room: RoomForm) => {
-            handleSubmitRoom(room);
+            dispatch(createRoomAction(room));
           }}
           handleError={(e) => {
             console.log(e);
