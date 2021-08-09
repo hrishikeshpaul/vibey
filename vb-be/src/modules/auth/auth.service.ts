5ac203a2aa91dc02b79a1c3e4eae8f4d3f1bef3e
Hrishikesh Paul <hrpaul@iu.edu>
2021-08-08 22:31:54 -0500

    chore: changed accesstoken exp

--- vb-be/src/modules/auth/auth.service.ts
+++ vb-be/src/modules/auth/auth.service.ts
@@ -10,7 +10,7 @@ import { ITokenUser, TokenTypes } from '@modules/auth/auth.constants';
 
 const accessOptions = {
   issuer: 'vibey',
-  expiresIn: '10s',
+  expiresIn: '1h',
 };
 
 const refreshOptions = {
