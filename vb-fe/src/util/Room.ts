600b492e3dba5f2972b5369f07d13bddfc457cbd
s-munro <sjm.munro@gmail.com>
2021-08-08 23:08:11 -0400

    chore: host object interface fleshed out

--- vb-fe/src/util/Room.ts
+++ vb-fe/src/util/Room.ts
@@ -8,7 +8,17 @@ export interface RoomForm {
 
 export interface dataCreateRoom {
   description: string;
-  host: { id: string; username: string };
+  host: {
+    id: string;
+    username: string;
+    uri: string;
+    likes: any[];
+    image: string;
+    href: string;
+    email: string;
+    display_name: string;
+    createdAt: string;
+  };
   name: string;
   start: Date;
   tags: Tag[];
