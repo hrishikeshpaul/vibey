f5afdef32c6515faf1fe29757ce26d2d3421652a
s-munro <sjm.munro@gmail.com>
2021-08-08 22:50:28 -0400

    ref: createRoomAction removed socket emit

--- vb-fe/src/_store/room/RoomActions.ts
+++ vb-fe/src/_store/room/RoomActions.ts
@@ -2,15 +2,12 @@ import { Dispatch } from "redux";
 import { createRoom } from "services/Room";
 import { RoomForm } from "util/Room";
 import { ILocalStorageUser } from "util/User";
-import { State } from "_store/rootReducer";
 import { SystemActionTypes, SystemConstants } from "_store/system/SystemTypes";
 import { RoomActionTypes, RoomConstants } from "./RoomTypes";
 
 export const createRoomAction =
   (room: RoomForm) =>
-  async (dispatch: Dispatch<RoomActionTypes | SystemActionTypes>, getState: () => State): Promise<void> => {
-    const { socket } = getState().system;
-
+  async (dispatch: Dispatch<RoomActionTypes | SystemActionTypes>): Promise<void> => {
     dispatch({
       type: SystemConstants.LOADING,
       payload: false,
@@ -19,29 +16,22 @@ export const createRoomAction =
     try {
       const user: ILocalStorageUser = JSON.parse(localStorage.getItem("v-user") || "");
       const res = await createRoom(room, user._id);
-      const { description, host, name, start, tags, _id } = res.data;
-      const roomData = {
-        description,
-        host: { id: host._id, username: host.username },
-        name,
-        start,
-        tags,
-        _id,
-      };
-      socket?.emit("create-room", { room: roomData });
 
       dispatch({
         type: RoomConstants.CREATE,
         payload: res.data,
       });
       dispatch({
-        type: SystemConstants.LOADING,
+        type: SystemConstants.CREATE_ROOM_MODAL,
         payload: false,
+      });
+      dispatch({
+        type: SystemConstants.SUCCESS,
       });
     } catch (err) {
       dispatch({
-        type: SystemConstants.LOADING,
-        payload: false,
+        type: SystemConstants.FAILURE,
+        payload: err,
       });
     }
   };
--- vb-fe/src/_store/rootReducer.ts
+++ vb-fe/src/_store/rootReducer.ts
@@ -1,17 +1,18 @@
 import { combineReducers } from "redux";
-// import { systemReducer } from "./system/systemReducer";
-// import { userReducer } from "./user/userReducer";
 import { systemReducer, SystemState } from "_store/system/SystemReducer";
 import { userReducer, UserState } from "_store/user/UserReducer";
+import { roomReducer, RoomState } from "_store/room/RoomReducer";
 
 export interface State {
   system: SystemState;
   user: UserState;
+  room: RoomState;
 }
 
 export const rootReducer = combineReducers({
   system: systemReducer,
   user: userReducer,
+  room: roomReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
