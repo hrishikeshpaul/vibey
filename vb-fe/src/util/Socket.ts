159cce3caa5ea4245a59197c76ac26d17ff04399
s-munro <sjm.munro@gmail.com>
2021-08-15 01:17:28 -0400

    chore: socket util with static infos

--- vb-fe/src/util/Socket.ts
+++ vb-fe/src/util/Socket.ts
@@ -1,3 +1,22 @@
 import { BASE_URL } from "./Endpoints";
 
 export const SOCKET_ENDPOINT = `${BASE_URL}`;
+export const HEROKU_SOCKET_ENDPOINT = "https://vibey-be--qa.herokuapp.com/";
+
+export enum SocketEvents {
+  Connect = "connect",
+  JoinRoom = "join-room",
+  JoinSuccess = "join-room-success",
+  Message = "message",
+  Exception = "exception",
+}
+
+export interface SocketError {
+  status: string;
+  message: string;
+}
+
+export interface SocketMessage {
+  message: string;
+  roomId: string;
+}
